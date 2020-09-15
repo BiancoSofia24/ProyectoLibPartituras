6c54f91706b7e460a1964e2ba4babade5762fef3
BiancoSofia24 <b.sofia2124@gmail.com>
2020-09-15 15:01:55 -0300

    Folders

---
+++ includes/nav.php
@@ -0,0 +1,30 @@
+<nav class="nav-menu sticky-top py-2">
+    <div class="container d-flex flex-row flex-md-row justify-content-between">
+        <a class="py-2" href="#">Home</a>
+
+        <a class="py-2" href="#">Services</a>
+
+        <a class="py-2" href="#">About</a>
+
+        <a class="py-2" href="#">User</a>
+
+        <button class="btn btn-dark">
+            <a href="#">
+                <i class="fas fa-sign-in-alt mr-2"></i> Login
+            </a>
+        </button>
+        
+        <!--
+            <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a href="#"><i class="fas fa-sign-out-alt"></i> Username</a>
+            </button>
+
+            <div class="dropdown-menu bg-dark" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item bg-dark" href="admin.php">Ir al dashboard</a>
+                <a class="dropdown-item bg-dark" href="#">Acción</a>
+                <a class="dropdown-item bg-dark" href="logout.php">Salir del sistema</a>
+            </div>
+         -->
+
+    </div>
+</nav>
---
+++ plantilla.php
@@ -0,0 +1,13 @@
+<?php  
+	include 'includes/head.html';
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Container</h1>
+
+    </main>
+
+<?php  
+    include 'includes/footer.php';
+?>
